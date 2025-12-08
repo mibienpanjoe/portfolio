321bb587720b86dc4050a7128f6b8b892d897a18
mibienpanjoe <epixprint00@gmail.com>
2025-12-08 15:14:26 +0000

    implement windows management with Zustand & immer

---
+++ src/store/window.js
@@ -0,0 +1,33 @@
+import { create } from 'zustand'
+import {immer} from 'zustand/middleware/immer'
+import {INITIAL_Z_INDEX, WINDOW_CONFIG} from "#constants/index.js";
+
+const useWindowStore = create(immer((set) => ({
+    windows : WINDOW_CONFIG ,
+    nextZindex : INITIAL_Z_INDEX + 1,
+
+    openWindow : (windowKey , data =null) => set((state) => {
+        const win = state.windows[windowKey];
+        if(!win) return;
+        win.isOpen = true;
+        win.zIndex = state.nextZindex++;
+        win.data = data ?? win.data;
+        state.nextZindex++;
+    }),
+
+    closeWindow : (windowKey) => set((state) => {
+        const win = state.windows[windowKey];
+        if(!win) return;
+        win.isOpen = false;
+        win.zIndex = INITIAL_Z_INDEX;
+        win.data = null;
+    }),
+
+    focusWindow : (windowKey) => set((state) => {
+        const win = state.windows[windowKey];
+        if(!win) return;
+        win.zIndex = state.nextZindex++;
+    })
+})));
+
+export default useWindowStore;
