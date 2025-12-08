726acf68624d8531b6dd7040559e053b2f02dc5c
mibienpanjoe <epixprint00@gmail.com>
2025-12-08 13:37:22 +0000

    implement dock component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/Welcome.jsx";
+import Dock from "#components/Dock.jsx";
 
-export { Welcome , Navbar };
+export { Welcome , Navbar , Dock };
